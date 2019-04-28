86eea7d1f58b6f1629ad8f8487d4059b3b0418d1
fung991159 <yeung.kei.fung@gmail.com>
2019-04-28 20:20:03 +0800

    completed Chapter 8

--- Chapter 8 – Reading and Writing Files.py
+++ Chapter 8 – Reading and Writing Files.py
@@ -2,44 +2,67 @@
 # Create a Mad Libs program that reads in text files 
 # and lets the user add their own text anywhere the word 
 # ADJECTIVE, NOUN, ADVERB, or VERB appears in the text file
-import fnmatch
-import string
-import re
 
-helloFile = open(r'C:\Users\Fung\Documents\vsc git repository\Automating-Boring-Stuff-with-Python-projects\text.txt')
-a = helloFile.readlines()
+def madLibs(): 
+    import re,os 
+    sourceFolder = r'C:\Users\Fung\Documents\vsc git repository\Automating-Boring-Stuff-with-Python-projects'
 
-#:read text file for keyword
-with open(r'C:\Users\Fung\Documents\vsc git repository\Automating-Boring-Stuff-with-Python-projects\text.txt') as f:
-    content = " ".join(line.strip() for line in f)
-    wordList = content.split()
+    with open(os.path.join(sourceFolder,'old_text.txt'),'r') as txt:
+        txtList = txt.readlines()
+
+    newlineList=[] 
+    numberofLine=len(txtList) #for omitting adding newline to last line
+    currentLine = 1
+    generalRegex = re.compile(r'adjective|noun|verb', re.IGNORECASE)
     
-keyWordList = ['ADJECTIVE', 'NOUN', 'VERB']
-cleanWordList =[]
-keyWordPos = []
-keyWordStr = []
-punRegex = re.compile(r'\W') #remove punctuation 
-for word in wordList:
-    cleanWordList.append(punRegex.sub('',word))
-i=0 
-for word in cleanWordList:
-    if word in keyWordList:
-        keyWordPos.append(i)
-        keyWordStr.append(word)
-    i+=1
+    for line in txtList:
+        eachLine = line.split()  #split each line in group
+        endOfList = len(eachLine)  #for addingback the newline
+        for word in eachLine:
+            mo = generalRegex.search(word)
+            if mo is not None:    #key words found
+                if mo.group().lower() == 'adjective':
+                    print('Enter an adjective:')
+                else:
+                    print(f'Enter a {mo.group().lower()}:')
+                userInput = input()
+                newlineList.append(userInput)
+            else:
+                newlineList.append(word)
+        if currentLine < numberofLine:  #add new line for each line except last line
+            currentLine += 1
+            # add newline to the last item in list to avoid extra space
+            newlineList[len(newlineList)-1] = newlineList[len(newlineList)-1] + "\n"
+        #write replaced string line into a new txt file
+        with open(os.path.join(sourceFolder,'new_text.txt'),'w') as newTxt:
+            print(newlineList)
+            newTxt.write(' '.join(newlineList))
 
-# create a input textfile with appropriate heading
-replaceStr=[]
-for i in range(len(keyWordStr)):
-    if keyWordStr[i] == 'ADJECTIVE':
-        print('Enter an adjective:')
-    else:
-        print('Enter a {}:'.format(keyWordStr[i].lower()))
-    replaceStr.append(input())
+# Regex Search
+# Write a program that opens all .txt files in a folder and searches 
+# for any line that matches a user-supplied regular expression.
+# The results should be printed to the screen.
 
-#The results should be printed to the screen and saved to a new text file.
-newWordList = []
+def regexSearch():
+    import os, re
+    print(r'Please input regular expression you want to search for:')
+    regexString = input()
+    searchRegex = re.compile(regexString)
+    targetFolder = r'C:\Users\Fung\Downloads\testGap'
+    keyWordFlag = False
+    
+    for folderName,subFolder,filenames in os.walk(targetFolder):
+        for file in filenames:
+            with open (os.path.join(folderName,file)) as f:
+                lineList = f.readlines()
+                for line in lineList:
+                    if searchRegex.search(line) is not None:
+                        keyWordFlag = True
+                        print (f'Keyword has been found in {os.path.join(folderName,file)}!')
 
-for i in range(len(keyWordStr)):
-    wordList = wordList[keyWordPos[i]]
-print (wordList)
+    if keyWordFlag == False:
+        print(f'There are no text matches "{regexString}!')
+
+if __name__ == "__main__":
+    # madLibs()
+    # regexSearch()
--- tempCodeRunnerFile.py
+++ tempCodeRunnerFile.py
@@ -1,1 +1,2 @@
-i
+   # with open(os.path.join(sourceFolder,'new_text.txt'),'w') as newTxt:
+    #     newTxt.write(' '.join(newlineList)
