c9b26f8c9eb1062702c9056a8a1c81a251764b56
fung991159 <yeung.kei.fung@gmail.com>
2019-05-13 19:36:21 +0800

    completed Ch15

--- Chapter 15 - Keeping Time, Scheduling Tasks, and Launching Programs.py
+++ Chapter 15 - Keeping Time, Scheduling Tasks, and Launching Programs.py
@@ -1,6 +1,9 @@
-#Prettified Stopwatch
-import time, pyperclip
 
+import time, pyperclip
+import requests, bs4, os, datetime, re
+
+
+#Prettified Stopwatch
 def pretty_stopwatch():
     print('Press ENTER to begin. Afterwards, press ENTER to "click" the stopwatch. " \
         Press Ctrl-C to quit.')
@@ -25,5 +28,39 @@ def pretty_stopwatch():
         # Handle the Ctrl-C exception to keep its error message from displaying.
         print('\nDone.')
 
+def Scheduled_Web_Comic_Downloader():
+    #Assume task schedule will run this program on a daily basis
+    #check if website has new comic
+    headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/50.0.2661.102 Safari/537.36'}
+    res = requests.get('https://www.buttersafe.com/', headers=headers)
+    res.raise_for_status()
+    soup = bs4.BeautifulSoup(res.text, 'html.parser')
+    dateElement = soup.select('#headernav-date')
+    latestReleaseDate_str = dateElement[0].text.strip() #string
+    #regex to take needed element
+    dateRegex = r'.* (\w*) (\d+).*, (\d{4})'
+    m = re.search(dateRegex, latestReleaseDate_str)
+    #turn month string into integer
+    latestReleaseDate = datetime.datetime.strptime(m.group(3)+m.group(1)+m.group(2), '%Y%b%d')
+
+    if datetime.datetime.now() != latestReleaseDate:
+        print('still the same old comic today :(')
+        pass #do nothing if latest comic release date isn't today
+    else:
+        print('There is new comic today :)')
+        #scrap image to folder
+        os.makedirs('buttersafe_comic', exist_ok=True)
+        picElement = soup.select('#comic img')
+        picUrl = picElement[0].get('src')
+        res = requests.get(picUrl, headers=headers)
+        res.raise_for_status()
+
+        with open(os.path.join('buttersafe_comic',os.path.basename(picUrl)), 'wb') as imageFile:
+            print(f'downloading {picUrl}')
+            for chunk in res.iter_content(100000):
+                imageFile.write(chunk)
+            imageFile.close()
+ 
 if __name__ == "__main__":
-    pretty_stopwatch()
+    # pretty_stopwatch()
+    # Scheduled_Web_Comic_Downloader()
